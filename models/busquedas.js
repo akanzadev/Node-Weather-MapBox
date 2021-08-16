be2bd078cdbc42babf59d8ad9160f12f2351f822
Jeancarlo <jeancarlosmamanilingan@gmail.com>
2021-08-16 09:27:55 -0500

    mapbox finish

--- models/busquedas.js
+++ models/busquedas.js
@@ -5,21 +5,28 @@ class Busquedas {
   /* historial = ["Tegucigalpa", "Madrid"]; */
   constructor() {}
   async ciudad(lugar = "") {
-    const instance = axios.create({
-      baseURL: `${config.MAPBOX.URI}/${lugar}.json`,
-      params: {
-        access_token: `${config.MAPBOX.TOKEN}`,
-        limit: config.MAPBOX.LIMIT,
-        language: `${config.MAPBOX.LANGUAGE}`,
-      },
-    });
-    const res = await instance.get();
-    return res.data.features.map((lugar) => ({
-      id: lugar.id,
-      nombre: lugar.place_name,
-      lng: lugar.center[0],
-      lat: lugar.center[1],
-    }));
+    try {
+      // Configurar axios
+      const instance = axios.create({
+        baseURL: `${config.MAPBOX.URI}/${lugar}.json`,
+        params: {
+          access_token: `${config.MAPBOX.TOKEN}`,
+          limit: config.MAPBOX.LIMIT,
+          language: `${config.MAPBOX.LANGUAGE}`,
+        },
+      });
+      // Petición http
+      const res = await instance.get();
+      return res.data.features.map((lugar) => ({
+        id: lugar.id,
+        nombre: lugar.place_name,
+        lng: lugar.center[0],
+        lat: lugar.center[1],
+      }));
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
   }
 }
 module.exports = Busquedas;
